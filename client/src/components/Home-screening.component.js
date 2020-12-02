bdd74e39ad8a9e87efcdba70e756b997afcf7382
bcsachin <sachinbhat.cg@gmail.com>
2020-12-03 00:47:14 +0530

    hello

--- client/src/components/Home-screening.component.js
+++ client/src/components/Home-screening.component.js
@@ -1,21 +1,86 @@
-//DisplayHello
-
 import React, { Component } from "react";
-//import axios from "axios";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const Exercise = (props) => (
+  <tr>
+    <td>{props.exercise.username}</td>
+    <td>{props.exercise.description}</td>
+    <td>{props.exercise.duration}</td>
+    <td>{props.exercise.date.substring(0, 10)}</td>
+    <td>
+      <Link to={"/edit/" + props.exercise._id}>edit</Link> |{" "}
+      <a
+        href="#"
+        onClick={() => {
+          props.deleteExercise(props.exercise._id);
+        }}
+      >
+        delete
+      </a>
+    </td>
+  </tr>
+);
 
 export default class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
 
-   
+    this.deleteExercise = this.deleteExercise.bind(this);
+
+    this.state = { exercises: [] };
+  }
+
+  componentDidMount() {
+    axios
+      .get("/exercises/")
+      .then((response) => {
+        this.setState({ exercises: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  deleteExercise(id) {
+    axios.delete("/exercises/" + id).then((response) => {
+      console.log(response.data);
+    });
+
+    this.setState({
+      exercises: this.state.exercises.filter((el) => el._id !== id),
+    });
+  }
+
+  exerciseList() {
+    return this.state.exercises.map((currentexercise) => {
+      return (
+        <Exercise
+          exercise={currentexercise}
+          deleteExercise={this.deleteExercise}
+          key={currentexercise._id}
+        />
+      );
+    });
+  }
+
   render() {
     return (
-        <div class="container">
-                 <h4 > BEE CREATIVE STUDIOS </h4>
-<img src="https://res.cloudinary.com/sachinbisgod/image/upload/v1600597090/images_movkne.jpg"/>
-
-
-
-        </div>
-    
+      <div>
+        <h3>Logged Exercises</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Username</th>
+              <th>Description</th>
+              <th>Duration</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>{this.exerciseList()}</tbody>
+        </table>
+      </div>
     );
   }
 }
