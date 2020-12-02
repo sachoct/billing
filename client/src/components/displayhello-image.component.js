06549c8a50c9e882c0381184a962a8ca5ba1e797
bcsachin <sachinbhat.cg@gmail.com>
2020-12-02 14:30:40 +0530

    first

--- client/src/components/displayhello-image.component.js
+++ client/src/components/displayhello-image.component.js
@@ -6,24 +6,25 @@ import axios from "axios";
 export default class DisplayHello extends Component {
   
     getUser(e) {
-        e.preventDefault();
+        alert("hello");
+        // e.preventDefault();
     
         
     
-        axios
-          .get("/users/")
-          .then((response) =>{
+        // axios
+        //   .get("/users/")
+        //   .then((response) =>{
 
-          if (response.data.length > 0) {
-            this.setState({
-              users: response.data.map((user) => user.username),
-              username: response.data[0].username,
-            }); }}
-          )
+        //   if (response.data.length > 0) {
+        //     this.setState({
+        //       users: response.data.map((user) => user.username),
+        //       username: response.data[0].username,
+        //     }); }}
+        //   )
         
 
 
-        return "Names";
+        // return "Names";
       }
   render() {
     return (
@@ -31,7 +32,7 @@ export default class DisplayHello extends Component {
                  <h4> Hello IMAGE </h4>
 <img src="https://res.cloudinary.com/sachinbisgod/image/upload/v1600597090/images_movkne.jpg"/>
 <h3>{this.getUser()}</h3><br></br>
-    <h3>{this.state.users[0]}</h3>
+    <h3></h3>
         </div>
     
     );
