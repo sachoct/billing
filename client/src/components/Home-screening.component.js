59f4ea671293393c2d2e41825bcbfae20123eca3
bcsachin <sachinbhat.cg@gmail.com>
2020-12-03 23:32:36 +0530

    first commit 3/12/third

--- client/src/components/Home-screening.component.js
+++ client/src/components/Home-screening.component.js
@@ -9,11 +9,13 @@ export default class HomeScreen extends Component {
   render() {
     return (
       <div>
-        <h4 id="wish" style={{color: "darkblue"},{textAlign: 'center'}}><b>  WEL-COME TO </b>  </h4>
-        <h4 id="nameoforg"  style={{color: "yellow"},{textAlign: 'center'}} > <b> BEE CREATIVE STUDIOS </b></h4>
-        <p> </p>
+        <div style={{backgroundColor: "indigo"}}>
+        <h4 id="wish" style={{color: "gold"},{textAlign: 'center'}}><b>  WEL-COME TO </b>  </h4>
+        <h4 id="nameoforg"  style={{color: "gold"},{textAlign: 'center'}} > <b> BEE CREATIVE STUDIOS </b></h4>
+        <p style={{color: "white"}} >  <br></br> Kannada Short films, Kannada Feature Films, Visual Effects Studios, Kannada Film Production House. </p>
+        </div>
         <h3 id="aboutheading" style={{color: "darkmagenta"}}> ABOUT</h3>
-        <p style={{color: "black"}} >  <br></br> BEE CREATIVE STUDIOS is the Kannada Short films, Kannada Feature Films, Visual Effects Studios, Kannada Film Production House. </p>
+       
         <p style={{color: "red"}}><br></br> watch this for better understanding <br></br>  </p>
        <ReactPlayer url= "https://youtu.be/zxqO87EWeNY" controls={true} width="100%" height = "280px"/>
 
