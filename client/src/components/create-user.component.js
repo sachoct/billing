b12f8a4877f938d5f24621774081cd1a6fb12565
sachoct <74526209+sachoct@users.noreply.github.com>
2020-12-08 11:48:53 +0530

    Update create-user.component.js

--- client/src/components/create-user.component.js
+++ client/src/components/create-user.component.js
@@ -16,7 +16,7 @@ export default class ExercisesList extends Component {
      
      <ReactPlayer url= "https://www.youtube.com/watch?v=9Eydvotl_nc" controls={true} width="100%" height = "280px"/>
     
-     <br></br><p><b>Wish You Happy Married Life Anna (RAJESH BHAT) </b></p>
+     <br></br><p><b>Wish You Happy Married Life Anna (RAJESH BHAT) Attige(DIVYA) </b></p>
       </div>
     );
   }
